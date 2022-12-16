b9609f1ed49c9ed179f654e1c3c2a46294387f51
Jonatas Lima <110483641+jonataspl@users.noreply.github.com>
2022-12-15 21:42:26 -0300

    Photos att

--- telescopes.js
+++ telescopes.js
@@ -2,7 +2,7 @@ let telescopeJson = [
   {
     id: 1,
     name: "Sv 550 APO 80mm",
-    img: "images/F9381A-SV550-11.jpg",
+    img: "images/F9381A-SV550-11.png",
     price: [4500.0, 6000.0, 6520],
     sizes: ["80mm F6.5", "Sv550 + Flattener", "Sv550 + Flattener + Rotador"],
     description:
@@ -11,7 +11,7 @@ let telescopeJson = [
   {
     id: 2,
     name: "Sv 503 ED",
-    img: "images/F9359D-SV503-ED-Telescope.jpg",
+    img: "images/F9359D-SV503-ED-Telescope.png",
     price: [4500.0, 6000.0, 6520],
     sizes: ["70mm", "80mm", "102mm"],
     description:
@@ -19,18 +19,18 @@ let telescopeJson = [
   },
   {
     id: 3,
-    name: "Sv 48p Achromatic 90mm",
-    img: "images/F9341A--10-Sv48P.jpg",
-    price: [289.99],
-    sizes: ["90mm F5.5"],
+    name: "Guider SvBony 60mm",
+    img: "images/W9155A---1-guide.png",
+    price: [449.99],
+    sizes: ["60mm"],
     description:
       "Descrição da Svbony em mais de uma linha muito legal bem interessante",
   },
   {
     id: 4,
     name: "Guider SvBony 50mm",
-    img: "images/W9155A---1-guide.jpg",
-    price: [149.99],
+    img: "images/W9155A---1-guide.png",
+    price: [349.99],
     sizes: ["50mm"],
     description:
       "Descrição da Svbony em mais de uma linha muito legal bem interessante",
@@ -38,7 +38,7 @@ let telescopeJson = [
   {
     id: 5,
     name: "Sv 405 CC OSC Cooled Camera",
-    img: "images/F9369A-SV405C--5.jpg",
+    img: "images/F9369A-SV405C--5.png",
     price: [4500.0],
     sizes: ["No filter"],
     description:
@@ -46,19 +46,37 @@ let telescopeJson = [
   },
   {
     id: 6,
-    name: "SV 305 Pro AR Planetary Camera",
-    img: "images/F9198C-9---1-sv305pro.jpg",
-    price: [1099.9],
+    name: "SV 305 Pro Planetary Camera",
+    img: "images/F9198C-9---1-sv305pro.png",
+    price: [990.9],
     sizes: ["UV/IR Filter"],
     description:
       "Descrição da Svbony em mais de uma linha muito legal bem interessante",
   },
   {
     id: 7,
-    name: "Sv 505c AR Planetary Camera",
-    img: "images/17Sv505c.jpg",
-    price: [299.99],
+    name: "Sv 605 CC OSC Cooled Camera",
+    img: "images/F9369A-SV405C--5.png",
+    price: [3499.99],
     sizes: ["No filter"],
+    description:
+      "Descrição da Svbony em mais de uma linha muito legal bem interessante",
+  },
+  {
+    id: 8,
+    name: "SV 305 Planetary Camera",
+    img: "images/F9198A-10--1--1-.png",
+    price: [650.0],
+    sizes: ["No filter"],
+    description:
+      "Descrição da Svbony em mais de uma linha muito legal bem interessante",
+  },
+  {
+    id: 9,
+    name: "Guider SvBony 30mm",
+    img: "images/W9125A-18-.png",
+    price: [219.99],
+    sizes: ["30mm"],
     description:
       "Descrição da Svbony em mais de uma linha muito legal bem interessante",
   },
